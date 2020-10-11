98203c4ff6849d8fcfb0ba3480c4d46376bb9f62
Jeff Shillitto <jeff@shillitto.com>
2020-10-11 22:33:28 +1100

    Add probe utility using exiftool to get media details

---
+++ src/utilities/Probe.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const exiftool = require("exiftool-vendored").exiftool
+
+/**
+ * Example:
+ * const probe = new Probe();
+ * probe.inspect('video.mp4').then((tags) => {
+ *   console.log(tags.get('ImageWidth'))
+ * })
+ */
+class Probe {
+    inspect(file) {
+        return new Promise((resolve, reject) => {
+            exiftool
+                .read(file)
+                .then((tags) => {
+                    exiftool.end()
+
+                    const response = {
+                        tags: tags,
+                        get: (tag) => {
+                            return tags[tag]
+                        },
+                        getWidth: () => {
+                            return tags.ImageWidth
+                        },
+                        getHeight: () => {
+                            return tags.ImageHeight
+                        },
+                        getDuration: () => {
+                            return tags.Duration
+                        },
+                        getRotation: () => {
+                            return tags.Rotation
+                        }
+                    }
+                    resolve(response)
+                })
+                .catch((err) => {
+                    console.error("Something terrible happened: ", err)
+                    reject(err)
+                })
+        })
+    }
+}
+
+module.exports.Probe = Probe;
